092a955080350e9212032ebc29da35643ee9f4c3
Martin J. Pollard <martin0566@gmail.com>
2022-05-18 21:45:35 -0400

    Settings: Integrate Altair Settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -538,4 +538,9 @@ public class Settings extends SettingsActivity {
     public static class ContentProtectionSettingsActivity extends SettingsActivity { /* empty */ }
     public static class MagnificationActivity extends SettingsActivity { /* empty */ }
     public static class FlashNotificationsActivity extends SettingsActivity { /* empty */ }
+
+    /**
+     * Activities for AltairSettings
+     */
+    public static class AltairSettingsActivity extends SettingsActivity {}
 }
